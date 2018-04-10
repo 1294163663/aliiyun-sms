482ede8d6055793722d553f3dddb6a78c72d3437
jim <1294163663@qq.com>
2018-04-10 20:36:01 +0800

    add function of receiving all the message

--- src/MsgServer.php
+++ src/MsgServer.php
@@ -46,14 +46,16 @@ class MsgServer
      * 获取消息
      * @param $config
      * @param string $messageType 消息类型
-     * @param string $queueName 在云通信页面开通相应业务消息后，就能在页面上获得对应的queueName<br/>(e.g. Alicom-Queue-xxxxxx-xxxxxReport)
      * @param callable $callback <p>
+     * @param array $config
+     * @return bool
      * 回调仅接受一个消息参数;
      * <br/>回调返回true，则工具类自动删除已拉取的消息;
      * <br/>回调返回false,消息不删除可以下次获取.
      * <br/>(e.g. function ($message) { return true; }
      * </p>
      */
+
     public static function receiveMsg($messageType, callable $callback, $config = [])
     {
         if ($config == []) {
@@ -89,69 +91,32 @@ class MsgServer
                         $receiptHandle = $res->getReceiptHandle();
                         $queue->deleteMessage($receiptHandle);
                     }
+                    return true;
                 }
 
-                return; // 整个取回执消息流程完成后退出
+                return false; // 整个取回执消息流程完成后退出
             }
             catch (MnsException $e)
             {
+                if ($e->getMnsErrorCode() == 404) {
+                    return false;
+                }
                 $i++;
                 echo "ex:{$e->getMnsErrorCode()}\n";
                 echo "ReceiveMessage Failed: {$e}\n";
             }
         }
+        return false;
     }
 
     public static function receiveSmsReport(callable $callback, $config = [])
     {
-        self::receiveMsg('SmsReport', $callback, $config);
+        return self::receiveMsg('SmsReport', $callback, $config);
     }
 
     public static function receiveSmsUp(callable $callback, $config = [])
     {
-        self::receiveMsg('SmsUp', $callback, $config);
+        return self::receiveMsg('SmsUp', $callback, $config);
     }
+
 }
-
-// 调用示例：
-
-//header('Content-Type: text/plain; charset=utf-8');
-//
-//echo "消息接口查阅短信状态报告返回结果:\n";
-//MsgServer::receiveMsg(
-//    // 消息类型，SmsReport: 短信状态报告
-//    "SmsReport",
-//
-//    // 在云通信页面开通相应业务消息后，就能在页面上获得对应的queueName
-//    "Alicom-Queue-1447497849932068-SmsReport",
-//
-//    /**
-//     * 回调
-//     * @param stdClass $message 消息数据
-//     * @return bool 返回true，则工具类自动删除已拉取的消息。返回false，消息不删除可以下次获取
-//     */
-//    function ($message) {
-//        print_r($message);
-//        return false;
-//    }
-//);
-//
-//
-//echo "消息接口查阅短信服务上行返回结果:\n";
-//MsgServer::receiveMsg(
-//    // 消息类型，SmsUp: 短信服务上行
-//    "SmsUp",
-//
-//    // 在云通信页面开通相应业务消息后，就能在页面上获得对应的queueName
-//    "Alicom-Queue-1447497849932068-SmsUp",
-//
-//    /**
-//     * 回调
-//     * @param stdClass $message 消息数据
-//     * @return bool 返回true，则工具类自动删除已拉取的消息。返回false，消息不删除可以下次获取
-//     */
-//    function ($message) {
-//        print_r($message);
-//        return false;
-//    }
-//);
